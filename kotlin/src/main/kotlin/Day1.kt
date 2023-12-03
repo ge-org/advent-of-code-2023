d989d3ad318e6e62edd991ad259b3d88d2e1f2da
Georg Dresler <georg@g-dresler.de>
2023-12-03 14:10:37 +0100

    Day 1 in Kotlin

--- kotlin/src/main/kotlin/Day1.kt
+++ kotlin/src/main/kotlin/Day1.kt
@@ -3,16 +3,40 @@ fun main() {
     part1("day1_puzzle").println().check(55002)
 
     part2("day1_2_sample").check(281)
-    part2("day1_puzzle").println()
+    part2("day1_puzzle").println().check(55093)
 }
 
-private fun part1(file: String): Int {
-    readLines(file)
+private fun part1(file: String): Int = readLines(file).sumFirstAndLastDigit()
 
-    TODO()
+private fun part2(file: String): Int = readLines(file).map {
+    if (!startsAndEndsWithDigit(it)) substituteFirstAndLastLiteralDigit(it) else it
+}.sumFirstAndLastDigit()
+
+private fun List<String>.sumFirstAndLastDigit(): Int = sumOf(::concatenateFirstAndLastDigit)
+
+private fun concatenateFirstAndLastDigit(line: String) = line.filter(Char::isDigit).let { "${it.first()}${it.last()}".toInt() }
+
+private fun startsAndEndsWithDigit(line: String) = line.first().isDigit() && line.last().isDigit()
+
+private fun substituteFirstAndLastLiteralDigit(line: String): String = line.mapIndexedNotNull { idx, char ->
+    when {
+        char.isDigit() -> char
+        else -> line.literalDigitsStartingAt(idx).firstNotNullOfOrNull { literalDigits[it] }
+    }
+}.joinToString()
+
+private fun String.literalDigitsStartingAt(start: Int): List<String> = (3..5).map { literalLength ->
+    substring(start, (start + literalLength).coerceAtMost(length))
 }
 
-private fun part2(file: String): Int {
-    TODO()
-}
-
+private val literalDigits = mapOf(
+    "one" to "1",
+    "two" to "2",
+    "three" to "3",
+    "four" to "4",
+    "five" to "5",
+    "six" to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine" to "9",
+)
--- kotlin/src/main/kotlin/funs.kt
+++ kotlin/src/main/kotlin/funs.kt
@@ -2,11 +2,11 @@ import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
 
-fun Any?.println(): Any? = this.also { println(this) }
+fun <T> T.println(): T = this.also { println(this) }
 
-fun Any?.check(expected: Any): Any? = this.also { kotlin.check(this == expected) }
+fun <T> T.check(expected: Any): T = this.also { kotlin.check(this == expected) }
 
-fun readLines(name: String) = Thread.currentThread().contextClassLoader
+fun readLines(name: String): List<String> = Thread.currentThread().contextClassLoader
     .getResourceAsStream(name).use { inputStream ->
         BufferedReader(InputStreamReader(inputStream as InputStream)).readLines()
     }
